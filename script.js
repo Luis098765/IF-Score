bda80102bc25d26c81323d1238172fac8ba1bfc1
Luís Fernando <cavalcantevieiral@gmail.com>
2025-01-17 21:41:08 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,51 @@
+function addSpace(step) {
+    let input = document.createElement("input");
+    input.classList.add(`N${step}-input`)
+    input.setAttribute('oninput', 'calcularMedia()')
+
+    let container = document.querySelector(`.N${step}-inputs`);
+
+    container.appendChild(input)
+}
+
+function removeSpace(step) {
+    let container = document.querySelector(`.N${step}-inputs`);
+
+    let inputs = container.querySelectorAll(`.N${step}-input`);
+    if (inputs.length > 1) { 
+        let lastInput = inputs[inputs.length - 1];
+        container.removeChild(lastInput);
+    }
+
+    calcularMedia()
+}
+
+function calcularMedia() {
+    let medias = [];
+
+    for (let i=0; i<4; i++) {
+        let soma = 0;
+        let container = document.querySelector(`.N${i+1}-inputs`);
+        let inputs = container.querySelectorAll(`.N${i+1}-input`);
+        
+        inputs.forEach(input => {
+            soma += Number(input.value);
+        })
+
+        let media = soma / inputs.length;
+
+        medias[i] = media;
+    }
+
+    let mediaFinal = ((medias[0]) + (medias[1]*2) + (medias[2]*3) + (medias[3]*4)) / 10;
+
+    let msg
+
+    if (mediaFinal >= 6) {
+        msg = "aprovado por média"
+    } else {
+        msg = "devendo nota"
+    }
+
+    document.getElementById("result").innerHTML = `Média final: ${mediaFinal} <br> Status: ${msg}`
+}
